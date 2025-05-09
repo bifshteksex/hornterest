5b427b401cd8ca722098d74446e36ea4da5a2fee
bifshteksex <roman_shangin@vk.com>
2025-05-09 17:38:19 +0800

    fix

--- internal/tasks/translate.go
+++ internal/tasks/translate.go
@@ -3,35 +3,87 @@ package tasks
 import (
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"net/http"
 	"net/url"
+	"time"
+
+	"pornterest/internal/models"
+
+	"gorm.io/gorm"
 )
 
-type TranslationTask struct {
-	TagID uint
+type TaskQueue struct {
+	translationTasks chan TranslationTask
+	db               *gorm.DB
+	logger           *slog.Logger
 }
 
-func (t *TaskQueue) AddTranslationTask(tagID uint) {
+type TranslationTask struct {
+	TagID int // Изменил тип с uint на int, чтобы соответствовать models.Tag
+}
+
+func NewTaskQueue(db *gorm.DB, logger *slog.Logger) *TaskQueue {
+	return &TaskQueue{
+		translationTasks: make(chan TranslationTask, 100),
+		db:               db,
+		logger:           logger,
+	}
+}
+
+func (t *TaskQueue) AddTranslationTask(tagID int) {
 	task := TranslationTask{TagID: tagID}
-	t.translationTasks <- task
+	select {
+	case t.translationTasks <- task:
+		t.logger.Info("added translation task", "tag_id", tagID)
+	default:
+		t.logger.Error("translation queue is full", "tag_id", tagID)
+	}
 }
 
 func (t *TaskQueue) ProcessTranslationTasks() {
 	for task := range t.translationTasks {
-		tag, err := t.db.GetTagByID(task.TagID)
-		if err != nil {
+		var tag models.Tag
+		if err := t.db.First(&tag, task.TagID).Error; err != nil {
+			t.logger.Error("failed to get tag",
+				"error", err,
+				"tag_id", task.TagID,
+			)
 			continue
 		}
 
-		// Используем MyMemory API для перевода
-		translatedText, err := translateText(tag.TitleEn)
-		if err != nil {
+		if tag.TitleEN == "" { // Обратите внимание на изменение TitleEn на TitleEN
+			t.logger.Error("tag has no English title", "tag_id", task.TagID)
 			continue
 		}
 
-		// Обновляем тег в БД
-		tag.TitleRu = translatedText
-		t.db.UpdateTag(tag)
+		translatedText, err := translateText(tag.TitleEN)
+		if err != nil {
+			t.logger.Error("failed to translate tag",
+				"error", err,
+				"tag_id", task.TagID,
+				"text", tag.TitleEN,
+			)
+			continue
+		}
+
+		tag.TitleRU = translatedText // Обратите внимание на изменение TitleRu на TitleRU
+		tag.UpdatedAt = time.Now()
+		if err := t.db.Save(&tag).Error; err != nil {
+			t.logger.Error("failed to update tag",
+				"error", err,
+				"tag_id", task.TagID,
+			)
+			continue
+		}
+
+		t.logger.Info("successfully translated tag",
+			"tag_id", tag.ID,
+			"from", tag.TitleEN,
+			"to", tag.TitleRU,
+		)
+
+		time.Sleep(time.Second)
 	}
 }
 
@@ -58,4 +110,8 @@ func translateText(text string) (string, error) {
 	}
 
 	return result.ResponseData.TranslatedText, nil
+}
+
+func (t *TaskQueue) StartProcessing() {
+	go t.ProcessTranslationTasks()
 }
